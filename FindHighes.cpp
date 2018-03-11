8536aafc1739f5809f335500a0da735c7727b63d
sonnyicks <35547157+sonnyicks@users.noreply.github.com>
2018-03-10 22:45:16 -0600

    PRACTICE

--- FindHighes.cpp
+++ FindHighes.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 ifstream doc;
+ofstream result;
 char filename[16];
 int number;
 int high;
@@ -18,10 +19,20 @@ int main(){
 		cout << "Cannot open file.";
 		exit (1);
 	}
+	result.open("answers.dat");
+	if (result.fail()){
+		cout << "Cannot write to file.";
+		exit (1);
+	}
 	find(doc);
 	cout << "Your highest number is " << high
-	<< " and your lowest number is " << low;
+	<< " and your lowest number is " << low << endl;
+	cout << "The average of the two is " << (low+high)/2;
+	result << "Your highest number is " << high
+	<< " and your lowest number is " << low << endl;
+	result << "The average of the two is " << (low+high)/2;
 	doc.close();
+	result.close();
 	return 0;
 }
 
