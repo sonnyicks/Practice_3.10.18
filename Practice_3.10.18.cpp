9ff2ef598ad0e70f6a08281abbe2a8ba9b736ddd
sonnyicks <35547157+sonnyicks@users.noreply.github.com>
2018-03-10 19:00:37 -0600

    continued practice

--- Practice_3.10.18.cpp
+++ Practice_3.10.18.cpp
@@ -1,20 +1,37 @@
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
+#include <iomanip>
 using namespace std;
 
 int main(){
 	int number;
 	ifstream in_stream;
 	ofstream out_stream;
-	int numone, numtwo, numthree, numfour;
+	double numone, numtwo, numthree, numfour;
+	char filename [16];
+	
+	cout << "input file name: ";
+	cin >> filename;
 	
 	in_stream.open("example.dat");
-	out_stream.open("outfile.dat");
+	if (in_stream.fail()){
+		cout << "The program exploded, Yo!";
+		exit (1);
+	}
+	out_stream.open(filename);
+	if (out_stream.fail()){
+		cout << "Read only, Yo!";
+		exit (1);
+	}
 	
 	in_stream >> numone >> numtwo >> numthree >> numfour;
 	
-	cout << numone << " " << numtwo << " " << numthree;
-	cout << " " << numfour;
+	cout.setf(ios::showpos);
+	cout.setf(ios::showpoint);
+	
+	cout << numone << " " << numtwo << setw(5) << " " << numthree;
+	cout << " " << setprecision(3) << numfour;
 	
 	out_stream << "The first number is " << numone << endl;
 	out_stream << "The second number is " << numtwo << endl;
